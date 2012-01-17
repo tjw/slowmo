7b2240c08f32ad2d09247d9c02d1fe63573e51bf
Timothy J. Wood <tjw@omnigroup.com>
2012-01-17 15:32:35 -0800

    Made this respect xcode-select -path.

--- build_slowmo.sh
+++ build_slowmo.sh
@@ -1,1 +1,14 @@
-gcc -arch i386 -lobjc -framework Foundation -bundle -o _slowmo slowmo.m
+#!/bin/zsh -euf
+
+dev_dir=`xcode-select -print-path`
+
+# Hack to find the latest SDK. Asked for real support in 10580379: xcodebuild should have flags to print the platform list and current sdk
+sdk_name=`/bin/ls "$dev_dir/Platforms/MacOSX.platform/Developer/SDKs" | tail -1`
+
+if [ -x "$dev_dir/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang" ]; then
+	CC="$dev_dir"/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
+else
+	CC="$dev_dir"/usr/bin/gcc
+fi
+	
+"$CC" -arch i386 -isysroot "$dev_dir/Platforms/MacOSX.platform/Developer/SDKs/$sdk_name" -lobjc -framework Foundation -bundle -o _slowmo slowmo.m
--- slowmo.sh
+++ slowmo.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
 SIM_PID=`ps x | grep "iPhone Simulator " | grep -v grep | awk '{print $1}'`
 if [ "$SIM_PID" == "" ]; then
 	echo "iPhone Simulator.app must be running to turn on slowmo."
 	exit 1;
 fi
 
-cat <<EOM | gdb -quiet > /dev/null
+dev_dir=`xcode-select -print-path`
+GDB="$dev_dir"/Platforms/iPhoneSimulator.platform/Developer/usr/bin/gdb
+
+cat <<EOM | "$GDB" -quiet > /dev/null
 attach $SIM_PID
 p (void *)dlopen("$(cd "$(dirname "$0")"; pwd)/_slowmo", 2)
 detach
